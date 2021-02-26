0a0cd0e42fb33359a63e0a8f76740695d0eeef2b
con3office <con3office@gmail.com>
2021-02-26 10:33:44 +0900

    Update install.sh
    fix dir

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ mv src/containers/backpack.jsx src/containers/backpack.jsx_orig
 mv src/lib/backpack-api.js src/lib/backpack-api.js_orig
 mv src/components/modal/modal.css src/components/modal/modal.css_orig
 mv src/playground/render-gui.jsx src/playground/render-gui.jsx_orig
-cp ${REPO_ID}/scratch-gui/src/containers/backpack.jsx src/containers/
-cp ${REPO_ID}/scratch-gui/src/lib/backpack-api.js src/lib/
-cp ${REPO_ID}/scratch-gui/src/components/modal/modal.css src/components/modal/
-cp ${REPO_ID}/scratch-gui/src/playground/render-gui.jsx src/playground/
+cp ${REPO_ID}/src/containers/backpack.jsx src/containers/
+cp ${REPO_ID}/src/lib/backpack-api.js src/lib/
+cp ${REPO_ID}/src/components/modal/modal.css src/components/modal/
+cp ${REPO_ID}/src/playground/render-gui.jsx src/playground/
